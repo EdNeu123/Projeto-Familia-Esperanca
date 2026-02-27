c93041a45cc759746d88f062be4933d9bcb1a8d4
Iago <iagorechtramontin@gmail.com>
2026-02-27 20:51:46 -0300

    Fix: Corrigido o mapa no rodape

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,54 +1,53 @@
-import { IoLogoWhatsapp } from 'react-icons/io5';
-import { IoMailOutline } from 'react-icons/io5';
-import { IoMapOutline } from 'react-icons/io5';
-import './styles.css';
+import { IoLogoWhatsapp } from "react-icons/io5";
+import { IoMailOutline } from "react-icons/io5";
+import { IoMapOutline } from "react-icons/io5";
+import "./styles.css";
 
 export default function Footer() {
-    return (
-        <footer className="footer-container">
-            <h3 className="footer-title">Entre em contato</h3>
-            <div className="footer-separator">
-                <div className="footer-info">
-                    <h3>Entre em contato</h3>
-                    <ul>
-                        <li>
-                            <IoLogoWhatsapp />
-                            <a
-                                href="https://wa.me/554738014163"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                (47) 3801-4163
-                            </a>
-                        </li>
-                        <li>
-                            <IoMailOutline />
-                            <a href="mailto:secretaria@familiaesperanca.com.br">
-                                secretaria@familiaesperanca.com.br
-                            </a>
-                        </li>
-                        <li>
-                            <IoMapOutline />
-                            <a
-                                href="https://www.google.com/maps/place/R.+Dona+Francisca,+3114+-+Saguaçu,+Joinville+-+SC,+89221-008"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                Família Esperança | Sede: Dona Francisca, 3114 -
-                                Saguaçu
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div className="footer-map">
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1228.830358185418!2d-48.846373506417!3d-26.27783622499856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deafd69319a03b%3A0xb3b9cfb2076d8be0!2sR.%20Dona%20Francisca%2C%203114%20-%20Sagua%C3%A7u%2C%20Joinville%20-%20SC%2C%2089221-008!5e0!3m2!1spt-PT!2sbr!4v1746204368326!5m2!1spt-PT!2sbr"
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
-                        title="Google Maps"
-                    ></iframe>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer-container">
+      <h3 className="footer-title">Entre em contato</h3>
+      <div className="footer-separator">
+        <div className="footer-info">
+          <h3>Entre em contato</h3>
+          <ul>
+            <li>
+              <IoLogoWhatsapp />
+              <a
+                href="https://wa.me/554738014163"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                (47) 3801-4163
+              </a>
+            </li>
+            <li>
+              <IoMailOutline />
+              <a href="mailto:secretaria@familiaesperanca.com.br">
+                secretaria@familiaesperanca.com.br
+              </a>
+            </li>
+            <li>
+              <IoMapOutline />
+              <a
+                href="https://www.google.com/maps/place/R.+Dona+Francisca,+3114+-+Saguaçu,+Joinville+-+SC,+89221-008"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Família Esperança | Sede: Dona Francisca, 3114 - Saguaçu
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-map">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1788.921516771842!2d-48.86862075313285!3d-26.266757328216574!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deafc14f5a0625%3A0xd92f26bdbbbe7a56!2zRmFtw61saWEgRXNwZXJhbsOnYQ!5e0!3m2!1spt-BR!2sbr!4v1772236185545!5m2!1spt-BR!2sbr"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="Google Maps"
+          ></iframe>
+        </div>
+      </div>
+    </footer>
+  );
 }
